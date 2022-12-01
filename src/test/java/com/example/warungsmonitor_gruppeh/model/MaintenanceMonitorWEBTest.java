70bf89053b7ff5249419718abd90cdfa6588313c
ic21b044@technikum-wien.at <nbwhiDZJaYRxVGTu6Lgs>
2022-12-01 19:20:35 +0100

    #13 testDefaultState implemented

--- src/test/java/com/example/warungsmonitor_gruppeh/model/MaintenanceMonitorWEBTest.java
+++ src/test/java/com/example/warungsmonitor_gruppeh/model/MaintenanceMonitorWEBTest.java
@@ -1,5 +1,19 @@
 package com.example.warungsmonitor_gruppeh.model;
 
+import com.example.warungsmonitor_gruppeh.service.MaintannanceMonitorService;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 public class MaintenanceMonitorWEBTest {
+
+
+    private final MaintannanceMonitorService monService = new MaintannanceMonitorService();
+
+    @Test
+    void testDefaultState() {
+        assertNull(null, monService.getMessage());
+    }
 
 }
